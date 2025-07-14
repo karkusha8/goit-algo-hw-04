0254d97ba2e924480b717e9a195d0dc691578a11
Oleksandr <chystykoleksand@gmail.com>
2025-07-14 10:46:10 +0300

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+def total_salary(path):
+    if not path.strip():
+        return "Шлях до файлу не вказано"
+
+    file_path = Path(path)
+
+    if not file_path.exists():
+        return "Файл не знайдено."
+
+    total = 0
+    count_developers = 0
+    with file_path.open('r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip().split(',')
+            if len(line) == 2 and line[0].strip() != '':
+                try:
+                    salary = float(line[1])
+                    total += salary
+                    count_developers += 1
+                except ValueError:
+                    continue
+
+    if count_developers == 0:
+        return "У файлі немає коректних зарплат."
+
+    average = total / count_developers
+    total = round(total, 2)
+    average = round(average, 2)
+    return total, average
+
+result = total_salary(r"Вказати шлях до файлу")
+
+if isinstance(result, tuple):
+    total, average = result
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+else:
+    print(result)
